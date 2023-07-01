ba0ffd22de919db1595f141471d7f9920bbaba55
Gustavo Larsen <guslarsen@gmail.com>
2023-07-01 10:41:14 -0300

    feat: pagina dar aulas

--- src/pages/GiveClasses/GiveClasses.tsx
+++ src/pages/GiveClasses/GiveClasses.tsx
@@ -1,8 +1,35 @@
-import { Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native';
+import giveClassesBgImage from '../../assets/images/give-classes-background.png';
+
+import styles from './styles';
 const GiveClasses = () => {
+  const { goBack } = useNavigation();
+
+  function hendleGoBack() {
+    goBack();
+  }
+
   return (
-    <View>
-      <Text>GiveClasses</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        resizeMode="contain"
+        source={giveClassesBgImage}
+        style={styles.content}
+      >
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor na nossa plataforma
+          web.
+        </Text>
+      </ImageBackground>
+
+      <TouchableOpacity
+        onPress={hendleGoBack}
+        style={styles.okButton}
+      >
+        <Text style={styles.okButtonText}>Tudo bem</Text>
+      </TouchableOpacity>
     </View>
   );
 };
